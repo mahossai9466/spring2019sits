b323d71409ceeb8e7f0f0fafff208f44e28a629e
al <al@LAPTOP-G9H4CJJM>
2019-04-18 12:02:40 -0400

    save

---
+++ sits_git/src/sits_git/hello.java
@@ -0,0 +1,11 @@
+package sits_git;
+
+public class hello {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ sits_git/src/sits_git/loop.java
@@ -0,0 +1,13 @@
+package sits_git;
+
+public class loop {
+
+	public static void main(String[] args) {
+		int x;
+	for ( x=1; x<10; x= x+2)
+		System.out.println(x);
+		// TODO Auto-generated method stub
+
+	}
+
+}
